f3158393ed48df6ca94cfc637bbe7c6a893ffb65
vojo28 <vojo28@gmail.com>
2026-05-02 20:06:59 +0100

    impact page

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,6 +1,16 @@
 // components/ui/container.tsx
 
-export default function Container({ children, className = "" }) {
+import { ReactNode } from "react";
+
+type ContainerProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export default function Container({
+  children,
+  className = "",
+}: ContainerProps) {
   return (
     <div
       className={`max-w-7xl mx-auto px-5 sm:px-6 md:px-8 lg:px-10 ${className}`}
