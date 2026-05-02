4faf7f68557b4d252b6d2a0656532163e99263d8
vojo28 <vojo28@gmail.com>
2026-05-02 20:08:36 +0100

    impact page

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -1,6 +1,16 @@
 // components/ui/section.tsx
 
-export default function Section({ children, className = "" }) {
+import { ReactNode } from "react";
+
+type SectionProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export default function Section({
+  children,
+  className = "",
+}: SectionProps) {
   return (
     <section
       className={`pt-12 md:pt-16 pb-16 md:pb-20 ${className}`}
